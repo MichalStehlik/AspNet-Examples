b099f592e2f77302146d0fd3c9843e10b71baeed
Michal Stehlík <michal.stehlik@pslib.cz>
2021-02-25 10:40:26 +0100

    simple links demo

---
+++ Links/Pages/Index.cshtml.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Links.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        public int A { get; set; }
+        public int B { get; set; }
+        public int Result { get; set; }
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet(int a = 0, int b = 0)
+        {
+            A = a;
+            B = b;
+            Result = A + B;
+        }
+    }
+}
