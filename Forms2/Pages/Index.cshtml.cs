877beb7d7e1b6a2eda29567d944563df879161eb
Michal Stehlík <michal.stehlik@pslib.cz>
2021-03-09 09:19:57 +0100

    Form2

---
+++ Forms2/Pages/Index.cshtml.cs
@@ -0,0 +1,49 @@
+﻿using Forms2.Model;
+using Forms2.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Forms2.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        [BindProperty]
+        public UserDataIM Input { get; set; } = new UserDataIM();
+
+        public List<SelectListItem> Classnames { get; set; }
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+            Classnames = new List<SelectListItem>
+            {
+                new SelectListItem { Text = "P1", Value = "p2020"},
+                new SelectListItem { Text = "P2", Value = "p2019"},
+                new SelectListItem { Text = "P3", Value = "p2018"},
+                new SelectListItem { Text = "P4", Value = "p2017"}
+            };
+        }
+
+        public void OnGet()
+        {
+            Input.Firstname = "Alfons";
+            Input.Agreement = true;
+            Input.Classname = "p2019";
+            Input.Gender = Gender.Female;
+        }
+
+        public IActionResult OnPost()
+        {
+            return Page();
+        }
+    }
+}
---
+++ Forms2/ViewModel/UserDataIM.cs
@@ -0,0 +1,24 @@
+﻿using Forms2.Model;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Forms2.ViewModel
+{
+    public class UserDataIM
+    {
+        [Required(ErrorMessage = "Pole jméno musí být vyplněno.")]
+        [Display(Name = "Jméno")]
+        public string Firstname { get; set; }
+        [Required(ErrorMessage = "Pole souhlas musí být vyplněno.")]
+        [Display(Name = "Souhlas")]
+        public bool Agreement { get; set; }
+        [Required]
+        public string Classname { get; set; }
+        [Required]
+        public Gender Gender { get; set; }
+    }
+}
